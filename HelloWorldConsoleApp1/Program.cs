45df32b4bd1234159044bcc1b4ec858192687519
ivan.basmanov.91@mail.ru <ivan.basmanov.91@mail.ru>
2022-05-18 23:04:51 +0700

    Variable

--- HelloWorldConsoleApp1/Program.cs
+++ HelloWorldConsoleApp1/Program.cs
@@ -8,3 +8,10 @@ Console.WriteLine(" World ");
 Console.WriteLine(" !!! ");
 Console.ReadLine();                // Задержка 
 
+
+var fullname = "Basmanov Ivan Alekseevich";
+var age = 31;
+var email = "ivan@mail.ru";
+var pointsProgram = 95.5;
+var pointsMathem = 90.2;
+var pointsPhisics = 92.7;
